0c64e2db9655f770e352a13594545dc81d7cb50e
10684056 <42819969+10684056@users.noreply.github.com>
2018-09-13 07:13:18 +0000

    finding the gcd of two numbers

---
+++ A4_10684056.cc
@@ -0,0 +1,18 @@
+#include<iuostream>
+using namespace std;
+int main()
+{int m,n;
+cout<<"Enter first number:";
+cin>>m;
+cout<<"Enter second number:";
+cin>>n;
+int mn=min(m,n);
+int gcd=1;
+for (int i=1;i<m,n;i++0)
+{if (m%i==0 && n%i==00
+{gcd =max (gcd,i)
+}
+}
+cout<<"greatest common divisor is:"<<gcd;
+reeturn 0;
+}
